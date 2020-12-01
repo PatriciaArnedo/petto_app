175437f771e185556bc6a5cbe3869da1c7df3e79
PatriciaArnedo <paaarnedo@gmail.com>
2020-12-01 18:31:17 -0500

    added pet and user render and fetches

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@
 //------elements------//
 const username = document.querySelector("#username")
 const userPets = document.querySelector("#pets-container")
+const petHunger = document.querySelector("#hunger")
+const petCleanliness = document.querySelector("#cleanliness")
+const petEnergy = document.querySelector("#energy")
+const petHappiness = document.querySelector("#happiness")
+const petImg = document.querySelector(".petimg")
+const petTitle = document.querySelector("#pet-title")
+const actionsTitle = document.querySelector("#actions-title")
+const friendList = document.querySelector("#friend-container")
 
 //-------fetches-------//
 
@@ -17,6 +25,24 @@ const userFetch = (id) => {
     })
 }
 
+const petFetch = (id) => {
+    fetch(`http://localhost:3000/api/pets/${id}`)
+    .then(r => r.json())
+    .then(petObj =>{
+        console.log(petObj)
+        renderPet(petObj)
+    })
+}
+
+const allPetFetch = () => {
+    fetch('http://localhost:3000/api/pets/')
+    .then(r => r.json())
+    .then(petArray => {
+        console.log(petArray)
+        petArray.forEach(pet => {renderFriend(pet)})
+    })
+}
+    
 
 //--------render functions----------//
 
@@ -32,14 +58,45 @@ const renderUserPet = (pet) => {
 
 
 
+const renderPet = (pet) =>{
+    petHunger.value = pet.hunger
+    petCleanliness.value = pet.cleanliness
+    petEnergy.value = pet.energy
+    petHappiness.value = pet.happiness
+
+    petImg.src = pet.happy_img
+    petTitle.textContent = pet.name
+    actionsTitle.textContent = `What Will You Do With ${pet.name}?`
+}
+
+const renderFriend = (pet) => {
+    let friendLi = document.createElement("li")
+    friendLi.textContent = pet.name
+    friendLi.dataset.id = pet.id
+    friendList.append(friendLi)
+}
+
+
+
 
 //---------event handlers----------//
+
+friendList.addEventListener("click", (event) => {
+    console.log(event)
+    if(event.target.tagName === 'LI' ){
+        console.log("list item clicked")
+        const id = event.target.dataset.id
+        petFetch(id)
+    }
+})
 
 
 
 
 //-----initialize------//
 
-userFetch(1)
+allPetFetch()
+userFetch(2)
+petFetch(7)
 
 
