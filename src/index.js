1ca03543cf866e89c7c83113758629ebdb957630
aru120 <khuang159@gmail.com>
2020-12-02 14:02:02 -0500

    working on logging in and rendering certain parts of page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 
+//------State-----//
+
+let petId = 0
+let currentUser = false
+
 
 
 //------elements------//
@@ -13,46 +18,82 @@ const petTitle = document.querySelector("#pet-title")
 const actionsTitle = document.querySelector("#actions-title")
 const friendList = document.querySelector("#friend-container")
 const petList = document.querySelector("#pets-container")
+const heartIcon = document.querySelector("#heart")
+const logIn = document.querySelector("#log-in")
+const petsContainerText = document.querySelector("#pets-container-text")
+const statsList = document.querySelector("#stats-list")
+const containerList = document.querySelector('#container-title')
+let displayButton = document.querySelectorAll(".button_display")
+
+
 
 
 //-------fetches-------//
 
 const userFetch = (id) => {
+    const btn = document.querySelector("#signup")
+
     fetch(`http://localhost:3000/api/users/${id}`)
-    .then(r => r.json())
-    .then(userObj => {
-        console.log(userObj)
-        renderUser(userObj)
-        userObj.pets.forEach(pet => {renderUserPet(pet)})
+        .then(r => r.json())
+        .then(userObj => {
+            console.log(userObj)
+            renderUser(userObj)
+            userObj.pets.forEach(pet => { renderUserPet(pet) })
+        })
+    currentUser = true
+
+
+    displayButton.forEach((button) => {
+        button.style.display = " "
     })
+    btn.style.display = "none"
+
 }
 
 const petFetch = (id) => {
     fetch(`http://localhost:3000/api/pets/${id}`)
-    .then(r => r.json())
-    .then(petObj =>{
-        console.log(petObj)
-        renderPet(petObj)
-    })
+        .then(r => r.json())
+        .then(petObj => {
+            console.log(petObj)
+            renderPet(petObj)
+        })
 }
 
 const allPetFetch = () => {
     fetch('http://localhost:3000/api/pets/')
-    .then(r => r.json())
-    .then(petArray => {
-        console.log(petArray)
-        petArray.forEach(pet => {renderFriend(pet)})
-    })
+        .then(r => r.json())
+        .then(petArray => {
+            console.log(petArray)
+            petArray.forEach(pet => { renderFriend(pet) })
+        })
 }
-    
+
 
 //--------render functions----------//
 
-const renderUser = ({name}) => {
+function renderButtons(buttonflag) {
+    if (!buttonflag) {
+        displayButton.forEach((button) => {
+            button.style.display = "none"
+        })
+
+    } else if (buttonflag) {
+
+        displayButton.forEach((button) => {
+            button.style.display = ""
+        })
+        btn.style.display = "none"
+    }
+}
+
+const renderUser = ({ name }) => {
     username.textContent = name
+    currentUser = true
+    renderButtons(currentUser)
 }
 
 const renderUserPet = (pet) => {
+    container = ""
     let petLi = document.createElement("li")
     petLi.textContent = pet.name
     petLi.dataset.id = pet.id
@@ -61,11 +102,19 @@ const renderUserPet = (pet) => {
 
 
 
-const renderPet = (pet) =>{
+const renderPet = (pet) => {
+    const petHappiness = document.createElement("progress")
+    const petHunger = document.createElement("progress")
+    const petCleanliness = document.createElement("progress")
+    const petEnergy = document.createElement("progress")
+    petId = pet.id
     petHunger.value = pet.hunger
     petCleanliness.value = pet.cleanliness
     petEnergy.value = pet.energy
     petHappiness.value = pet.happiness
+
+    statsList.innerHTML = ""
+    statsList.append(petHappiness, petHunger, petCleanliness, petEnergy)
 
     petImg.src = pet.happy_img
     petTitle.textContent = pet.name
@@ -73,6 +122,7 @@ const renderPet = (pet) =>{
 }
 
 const renderFriend = (pet) => {
+    petsContainerText.textContent = "Your Pets"
     let friendLi = document.createElement("li")
     friendLi.textContent = pet.name
     friendLi.dataset.id = pet.id
@@ -86,7 +136,7 @@ const renderFriend = (pet) => {
 
 friendList.addEventListener("click", (event) => {
     console.log(event)
-    if(event.target.tagName === 'LI' ){
+    if (event.target.tagName === 'LI') {
         console.log("list item clicked")
         const id = event.target.dataset.id
         petFetch(id)
@@ -95,19 +145,54 @@ friendList.addEventListener("click", (event) => {
 
 petList.addEventListener("click", (event) => {
     console.log(event)
-    if(event.target.tagName === 'LI' ){
+    if (event.target.tagName === 'LI') {
         console.log("list item clicked")
         const id = event.target.dataset.id
         petFetch(id)
     }
 })
 
+heartIcon.addEventListener("click", () => {
+    fetch(`http://localhost:3000/api/pets/${petId}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            happiness: happiness + 20
+        }),
+    })
+        .then(response => response.json())
+        .then(data => {
+
+            renderPet(data)
+        })
+})
 
 
+logIn.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const userName = e.target.name.value 
+    fetch(`http://localhost:3000/api/users/${userName}`)
+    .then(r => r.json())
+    .then(loginUser => {
+        
+        currentUser = true
+        renderUser(loginUser)
+
+        if(loginUser.pet){
+        loginUser.pets.forEach(pet =>{
+            renderPet(pet)
+        })
+    }
+    
+        allPetFetch()
+    })
+})
 //-----initialize------//
 
-allPetFetch()
-userFetch(2)
-petFetch(15)
-
+// allPetFetch()
+// // userFetch(2)
+// petFetch(25)
+renderButtons(currentUser)
 
--- src/modal.js
+++ src/modal.js
@@ -1,8 +1,12 @@
 const modal = document.querySelector("#modal")
 const btn = document.querySelector("#signup")
+
 document.querySelector("#signup").addEventListener("click", () => {
   modal.style.display = "block"
+  userFetch(1)
+  
 })
+
 // Hide the form
 modal.addEventListener("click", e => {
   if (e.target.dataset.action === "close") {
