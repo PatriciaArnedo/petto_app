12b9859ee33eb34bd129bccb417f7f489c6407eb
JlordA <james.ardery@gmail.com>
2020-12-03 17:01:41 -0500

    pet stats done

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@
 
 let petId = 0
 let currentUser = false
-
+let petCurrent
 
 
 //------elements------//
 const username = document.querySelector("#username")
 const userPets = document.querySelector("#pets-container")
-const petHunger = document.querySelector("#hunger")
-const petCleanliness = document.querySelector("#cleanliness")
-const petEnergy = document.querySelector("#energy")
-const petHappiness = document.querySelector("#happiness")
+const petHunger = document.querySelector("#hunger-progress")
+const petCleanliness = document.querySelector("#clean-progress")
+const petEnergy = document.querySelector("#energy-progress")
+const petHappiness = document.querySelector("#happy-progress")
 const petImg = document.querySelector(".petimg")
 const petTitle = document.querySelector("#pet-title")
 const actionsTitle = document.querySelector("#actions-title")
@@ -27,8 +27,8 @@ let displayButton = document.querySelectorAll(".button_display")
 const userDiv = document.querySelector("#user-div")
 const htmlBody = document.querySelector("body")
 const logOutBtn = document.querySelector("#log-out")
-
-
+const statBtns = document.querySelector("#button-container")
+const background = document.querySelector(".petbackground")
 
 //-------fetches-------//
 
@@ -38,7 +38,6 @@ const userFetch = (id) => {
     fetch(`http://localhost:3000/api/users/${id}`)
         .then(r => r.json())
         .then(userObj => {
-            console.log(userObj)
             renderUser(userObj)
             userObj.pets.forEach(pet => { renderUserPet(pet) })
         })
@@ -55,9 +54,8 @@ const userFetch = (id) => {
 const petFetch = (id) => {
     fetch(`http://localhost:3000/api/pets/${id}`)
         .then(r => r.json())
-        .then(petObj => {
-            console.log(petObj)
-            renderPet(petObj)
+        .then(petObject => {
+            renderPet(petObject)
         })
 }
 
@@ -65,10 +63,10 @@ const allPetFetch = () => {
     fetch('http://localhost:3000/api/pets/')
         .then(r => r.json())
         .then(petArray => {
-            console.log(petArray)
-            petArray.forEach(pet => { 
+            petArray.forEach(pet => {
                 containerList.textContent = ""
-                renderFriend(pet) })
+                renderFriend(pet)
+            })
         })
 }
 
@@ -76,8 +74,8 @@ const allPetFetch = () => {
 //--------render functions----------//
 
 function renderButtons(buttonflag) {
-    
-    
+
+
     if (!buttonflag) {
         displayButton.forEach((button) => {
             button.style.display = "none"
@@ -109,16 +107,21 @@ const renderUserPet = (pet) => {
 
 
 const renderPet = (pet) => {
+    petCurrent = pet
     const happyLI = document.createElement("li")
     const hungerLI = document.createElement("li")
     const energyLI = document.createElement("li")
     const cleanLI = document.createElement("li")
 
     const petHappiness = document.createElement("progress")
+    petHappiness.id = "happy-progress"
     const petHunger = document.createElement("progress")
+    petHunger.id = "hunger-progress"
     const petCleanliness = document.createElement("progress")
+    petCleanliness.id = "clean-progress"
     const petEnergy = document.createElement("progress")
-    
+    petEnergy.id = "energy-progress"
+
     petHunger.value = 0
     petCleanliness.value = 0
     petEnergy.value = 0
@@ -138,8 +141,8 @@ const renderPet = (pet) => {
     hungerLI.textContent = "Hunger:"
     energyLI.textContent = "Energy:"
     cleanLI.textContent = "Cleanliness:"
-    
-    
+
+
     happyLI.append(petHappiness)
     hungerLI.append(petHunger)
     energyLI.append(petEnergy)
@@ -167,40 +170,36 @@ const renderFriend = (pet) => {
 
 
 logOutBtn.addEventListener("click", () => {
-       location.reload()
-    })
+    location.reload()
+})
 
 friendList.addEventListener("click", (event) => {
-    console.log(event)
     if (event.target.tagName === 'LI') {
-        console.log("list item clicked")
         const id = event.target.dataset.id
         petFetch(id)
     }
 })
 
 petList.addEventListener("click", (event) => {
-    console.log(event)
     if (event.target.tagName === 'LI') {
-        console.log("list item clicked")
         const id = event.target.dataset.id
         petFetch(id)
     }
 })
 
 heartIcon.addEventListener("click", () => {
+
     fetch(`http://localhost:3000/api/pets/${petId}`, {
         method: 'PATCH',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            happiness: happiness + 20
+            happiness: petCurrent.happiness + 20
         }),
     })
         .then(response => response.json())
         .then(data => {
-
             renderPet(data)
         })
 })
@@ -208,28 +207,92 @@ heartIcon.addEventListener("click", () => {
 
 logIn.addEventListener("submit", (e) => {
     e.preventDefault()
-    const userName = e.target.name.value 
+    const userName = e.target.name.value
 
-    if (!userName){
+    if (!userName) {
         alert("Enter Username")
     }
-    else{
-    fetch(`http://localhost:3000/api/users/${userName}`)
-    .then(r => r.json())
-    .then(loginUser => {
-        
-        currentUser = true
-        renderUser(loginUser)
-        
-            loginUser.pets.forEach(pet =>{
-                renderUserPet(pet)
+    else {
+        fetch(`http://localhost:3000/api/users/${userName}`)
+            .then(r => r.json())
+            .then(loginUser => {
+
+                currentUser = true
+                renderUser(loginUser)
+
+                loginUser.pets.forEach(pet => {
+                    renderUserPet(pet)
+                })
+
+                renderPet(loginUser.pets[0])
+
+                allPetFetch()
             })
-        
-        renderPet(loginUser.pets[0])
-            
-        allPetFetch()
+    }
+})
+
+statBtns.addEventListener("click", (e) => {
+    const petHunger = document.querySelector("#hunger-progress")
+    const petCleanliness = document.querySelector("#clean-progress")
+    const petEnergy = document.querySelector("#energy-progress")
+    const petHappiness = document.querySelector("#happy-progress")
+
+    let hunger = petHunger.value
+    let energy = petEnergy.value
+    let happiness = petHappiness.value
+    let cleanliness = petCleanliness.value
+
+    if (e.target.dataset.id === "Feed") {
+        if (petHunger.value < 100 || petCleanliness.value > 0) {
+            hunger = hunger + 10
+            cleanliness = cleanliness - 10
+        }
+        petObject = {
+            hunger: hunger,
+            cleanliness: cleanliness
+        }
+    } else if (e.target.dataset.id === "Play") {
+        if (petEnergy.value > 0 || petCleanliness.value > 0) {
+            energy = energy - 10
+            cleanliness = cleanliness - 10
+        }
+        petObject = {
+            energy: energy,
+            cleanliness: cleanliness
+        }
+    } else if (e.target.dataset.id === "Clean") {
+        if (petCleanliness.value < 100 || petHappiness.value > 0) {
+            cleanliness = cleanliness + 20
+            happiness = happiness - 20
+        }
+        petObject = {
+            cleanliness: cleanliness,
+            happiness: happiness
+        }
+    } else if (e.target.dataset.id === "Rest") {
+        background.src = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/eaeb2983-14d8-43be-afa3-f21a68d73b60/d6me6v4-369be347-5b26-4611-9109-18dfd9a3b8d7.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvZWFlYjI5ODMtMTRkOC00M2JlLWFmYTMtZjIxYTY4ZDczYjYwXC9kNm1lNnY0LTM2OWJlMzQ3LTViMjYtNDYxMS05MTA5LTE4ZGZkOWEzYjhkNy5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.mYt3fEe_qI4lpwUYZqkSfNWi4tAO-hRfC5dRJfsE6JM"
+
+        if (petEnergy.value < 100 || petHunger.value > 0) {
+            energy = energy + 20
+            hunger = hunger - 20
+        }
+        petObject = {
+            energy: energy,
+            hunger: hunger
+        }
+    }
+    console.log(petId)
+    fetch(`http://localhost:3000/api/pets/${petId}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(petObject),
     })
-}
+        .then(response => response.json())
+        .then(newPetData => {
+            renderPet(newPetData);
+        })
 })
 //-----initialize------//
 
