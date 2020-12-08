7e63be43aae29cc7de7ca86129ac64a7308cab42
PatriciaArnedo <paaarnedo@gmail.com>
2020-12-07 22:38:16 -0500

    fixed main background repeating

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function sound(src) {
     this.sound.setAttribute("preload", "auto");
     this.sound.setAttribute("controls", "none");
     this.sound.style.display = "none";
-    this.sound.volume = 0.02;
+    this.sound.volume = 0.05;
     document.body.appendChild(this.sound);
     this.play = function(){
         this.sound.play();
